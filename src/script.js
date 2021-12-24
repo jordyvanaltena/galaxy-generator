7b6f1b0bfb972260969616ca265e93be5ceb3e4d
Jordy van Altena <jordy.van.altena@infocaster.net>
2021-12-24 17:01:58 +0100

    first commit

---
+++ src/script.js
@@ -0,0 +1,215 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'lil-gui'
+
+/**
+ * Base
+ */
+// Debug
+const gui = new dat.GUI()
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// /**
+//  * Test cube
+//  */
+// const cube = new THREE.Mesh(
+//     new THREE.BoxGeometry(1, 1, 1),
+//     new THREE.MeshBasicMaterial()
+// )
+// scene.add(cube)
+
+/**
+ * Galaxy
+ */
+const parameters = {}
+parameters.count = 100000
+parameters.size = 0.01
+parameters.radius = 5
+parameters.branches = 3
+parameters.spin = 1
+parameters.randomness = 0.2
+parameters.randomnessPower = 3
+parameters.insideColor = '#ff6030'
+parameters.outsideColor = '#1b3984'
+parameters.rotation = false;
+parameters.wave = false;
+
+
+let galaxyGeometry = null
+let galaxyMaterial = null
+let galaxyPoints = null
+
+const generateGalaxy = () => {
+    /**
+     * Geometry
+     */
+    if(galaxyPoints !== null){
+        galaxyGeometry.dispose()
+        galaxyMaterial.dispose()
+        scene.remove(galaxyPoints)
+    }
+    galaxyGeometry = new THREE.BufferGeometry()
+    const positions = new Float32Array(parameters.count  * 3)
+    const colors = new Float32Array(parameters.count  * 3)
+
+    const colorInside = new THREE.Color(parameters.insideColor)
+    const colorOutside = new THREE.Color(parameters.outsideColor)
+
+    
+    for (let i = 0; i < parameters.count; i++) {
+        const i3 = i * 3
+        
+        // Position
+        const radius = Math.random() * parameters.radius
+        const spin = radius * parameters.spin
+        const branchAngle = (i % parameters.branches) / parameters.branches * Math.PI * 2
+        
+        const randomX = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : -1) * parameters.randomness * radius
+        const randomY = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : -1) * parameters.randomness * radius
+        const randomZ = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : -1) * parameters.randomness * radius
+
+        positions[i3    ] = Math.cos(branchAngle + spin) * radius + randomX
+        positions[i3 + 1] = 0 + randomY
+        positions[i3 + 2] = Math.sin(branchAngle + spin) * radius + randomZ
+        
+        const mixedColor = colorInside.clone()
+        mixedColor.lerp(colorOutside, radius / parameters.radius)
+        // Color
+        colors[i3    ] = mixedColor.r;
+        colors[i3 + 1] = mixedColor.g;
+        colors[i3 + 2] = mixedColor.b;
+    }
+
+    galaxyGeometry.setAttribute(
+        'position',
+        new THREE.BufferAttribute(positions, 3)
+    )
+
+    galaxyGeometry.setAttribute(
+        'color',
+        new THREE.BufferAttribute(colors, 3)
+    )
+    
+    /**
+     * Material
+     */
+    galaxyMaterial = new THREE.PointsMaterial({
+        size: parameters.size,
+        sizeAttenuation: true,
+        depthWrite: false,
+        blending: THREE.AdditiveBlending,
+        vertexColors: true
+        // color: '#ff5588'
+    })
+
+
+    /**
+     * Points
+     */
+    galaxyPoints = new THREE.Points(galaxyGeometry, galaxyMaterial)
+    scene.add(galaxyPoints)
+}
+generateGalaxy()
+
+
+/**
+ * GUI
+ */
+gui.add(parameters, 'count').min(100).max(100000).step(100).onFinishChange(generateGalaxy)
+gui.add(parameters, 'size').min(.001).max(.1).step(.001).onFinishChange(generateGalaxy)
+gui.add(parameters, 'radius').min(.5).max(20).step(.01).onFinishChange(generateGalaxy)
+gui.add(parameters, 'branches').min(2).max(20).step(1).onFinishChange(generateGalaxy)
+gui.add(parameters, 'spin').min(-5).max(5).step(0.001).onFinishChange(generateGalaxy)
+gui.add(parameters, 'randomness').min(0).max(2).step(0.001).onFinishChange(generateGalaxy)
+gui.add(parameters, 'randomnessPower').min(1).max(10).step(0.001).onFinishChange(generateGalaxy)
+gui.addColor(parameters, 'insideColor').onFinishChange(generateGalaxy)
+gui.addColor(parameters, 'outsideColor').onFinishChange(generateGalaxy)
+gui.add(parameters, 'wave').onFinishChange(generateGalaxy)
+gui.add(parameters, 'rotation').onFinishChange(generateGalaxy)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 3
+camera.position.y = 3
+camera.position.z = 3
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    if(parameters.wave){
+        for(let i = 0; i < parameters.count; i++){
+            const i3 = i * 3
+            const xOffset = galaxyGeometry.attributes.position.array[i3]
+            const yOffset = galaxyGeometry.attributes.position.array[i3+2]
+            galaxyGeometry.attributes.position.array[i3+1] = Math.sin((elapsedTime) + Math.sin(xOffset) + Math.sin(yOffset))
+        }
+        galaxyGeometry.attributes.position.needsUpdate = true
+        
+    }
+    if(parameters.rotation){
+        galaxyPoints.rotation.y = elapsedTime * .1
+    }
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
